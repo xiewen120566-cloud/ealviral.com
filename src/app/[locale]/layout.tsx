fb7d32aaf394c2cfd7a2ad0471ba292dcde708e3
谢文 <1205669226@qq.com>
2026-06-04 09:11:54 +0800

    merge

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -63,7 +63,7 @@ var e=ttq._i[t]||[],n=0;n<ttq.methods.length;n++)ttq.setAndDefer(e,ttq.methods[n
 ;n.type="text/javascript",n.async=!0,n.src=r+"?sdkid="+e+"&lib="+t;e=document.getElementsByTagName("script")[0];e.parentNode.insertBefore(n,e)};
 
 
-  ttq.load('D884TSJC77UDONQHT2A0');
+  ttq.load('D8G3DA3C77U91RGD62DG');
   ttq.page();
 }(window, document, 'ttq');
           `,
